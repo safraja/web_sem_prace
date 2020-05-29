3e8033916c7b89f30a0d78db5854aec088966c2c
safraja <63454672+safraja@users.noreply.github.com>
2020-05-29 04:16:54 +0200

    Delete db.php

--- db.php
+++
@@ -1,7 +0,0 @@
-<?php
-/** @var \PDO $db - připojení k databázi */
-$db = new PDO('mysql:host=127.0.0.1;dbname=safj05;charset=utf8', 'safj05', 'zaedec4iethahN9eiz');
-//TODO nezapomeňte v předchozím řádku zadat své xname a heslo k databázi
-
-//při chybě v SQL chceme vyhodit Exception
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
